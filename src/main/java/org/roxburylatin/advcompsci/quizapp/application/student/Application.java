9fc61565c2cfd7a41cd2f50d2bc8a81ef79a2294
avishkumar <avish.kumar@roxburylatin.org>
2025-04-11 12:34:35 -0400

    Rename files

--- src/main/java/org/roxburylatin/advcompsci/quizapp/application/student/Application.java
+++ src/main/java/org/roxburylatin/advcompsci/quizapp/application/student/Application.java
@@ -1,19 +1,19 @@
 package org.roxburylatin.advcompsci.quizapp.application.student;
 
 import java.io.IOException;
-import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class StudentApplication extends Application {
+public class Application extends javafx.application.Application {
   public static void main(String[] args) {
     launch();
   }
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(StudentApplication.class.getResource("student-question-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("question-view.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 320, 240);
     stage.setTitle("Hello!");
     stage.setScene(scene);
