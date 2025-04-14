3b2d54aa3ec29fe7ed13244dd23ecaacc67b23f5
marcovsuri <86528815+marcovsuri@users.noreply.github.com>
2025-04-14 08:58:06 -0400

    Add quit on close primary window

--- src/main/java/org/roxburylatin/advcompsci/quizapp/application/teacher/Application.java
+++ src/main/java/org/roxburylatin/advcompsci/quizapp/application/teacher/Application.java
@@ -1,7 +1,7 @@
 package org.roxburylatin.advcompsci.quizapp.application.teacher;
 
 import java.io.IOException;
-
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -14,18 +14,24 @@ public class Application extends javafx.application.Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader =
-        new FXMLLoader(Application.class.getResource("teacher-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("teacher-view.fxml"));
     Scene scene = new Scene(fxmlLoader.load());
 
     // Load the CSS file
-    String cssPath =
-        Application.class.getResource("teacher-view-styles.css").toExternalForm();
+    String cssPath = Application.class.getResource("teacher-view-styles.css").toExternalForm();
     scene.getStylesheets().add(cssPath);
 
     stage.setTitle("Quiz App (Teacher View)");
     stage.setResizable(false);
     stage.setScene(scene);
     stage.show();
+
+    // Quit app when primary window closes
+    stage.setOnCloseRequest(
+        event -> {
+          System.out.println("Shutting down...");
+          Platform.exit(); // Clean JavaFX shutdown
+          System.exit(0); // Optional: kill any stray threads (use carefully)
+        });
   }
 }
